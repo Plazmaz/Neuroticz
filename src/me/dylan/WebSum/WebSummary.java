16b27c139daaaf4e64f64c6bed3e4f893600fd6a
Dylan Katz <dylanishappy1@gmail.com>
2014-02-16 06:03:26 -0700

    Done.

--- src/me/dylan/WebSum/WebSummary.java
+++ src/me/dylan/WebSum/WebSummary.java
@@ -1,27 +1,38 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+package me.dylan.WebSum;
 
+import java.util.HashMap;
 
-public class FileInput {
-	public static void main(String args[]){
-	
-	BufferedReader in = null;
-	try {
-		in = new BufferedReader(new FileReader("C:\\Users\\Hunter\\Desktop\\Server\\server.properties"));
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} 
-	try {
-		String text = in.readLine();
-	} catch (IOException e) {
-		e.printStackTrace();
-	} 
-	try {
-		in.close();
-	} catch (IOException e) {
-		e.printStackTrace();
+public class WebSummary {
+	public static void main(String args[]) {
+		//FileUtil.parseFile("Recipe.txt");
+		FileUtil util = new FileUtil();
+		HashMap<String, Integer> instances = util.learnWordImportance("recipes");
+		System.out.println("FileCount: " + util.fileCount);
+		for(String s : instances.keySet()) {
+			int wordAmt = instances.get(s);
+			float wordWeight = (wordAmt/(float)util.fileCount)/100;
+			System.out.println(s + " WordOccurences:" + wordAmt +
+					" WordWeight:" + wordWeight);
+			//System.out.println(s + " : " + (float)(instances.get(s)/util.fileCount));
+		}
+		/*
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(
+					"C:\\Users\\Hunter\\Desktop\\Server\\server.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			String text = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+
 	}
-
-	}
+}
