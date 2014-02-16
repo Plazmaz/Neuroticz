398677ac5b25307220d56be1c4d6ac97781bf7b3
Agent531C <Game.guy7000@gmail.com>
2014-02-15 20:08:03 -0700

    InputOutputFile

--- src/me/dylan/WebSum/WebSummary.java
+++ src/me/dylan/WebSum/WebSummary.java
@@ -1,5 +1,27 @@
-package me.dylan.WebSum;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class WebSummary {
 
-}
+public class FileInput {
+	public static void main(String args[]){
+	
+	BufferedReader in = null;
+	try {
+		in = new BufferedReader(new FileReader("C:\\Users\\Hunter\\Desktop\\Server\\server.properties"));
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} 
+	try {
+		String text = in.readLine();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} 
+	try {
+		in.close();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+
+	}
