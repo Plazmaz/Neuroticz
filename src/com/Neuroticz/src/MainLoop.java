4c63de2d17b8f21cf57a4853bc67b7e435e9cb95
Craig Gleckman <cgleckman@gmail.com>
2014-03-05 12:36:32 -0700

    About 1/3 through refactoring. Added TODOs where questions/concerns came up.
    Some areas of each file may still be incomplete.

--- src/com/Neuroticz/src/MainLoop.java
+++ src/com/Neuroticz/src/MainLoop.java
@@ -1,7 +1,6 @@
 package com.Neuroticz.src;
 
 import java.util.ArrayList;
-
 import me.dylan.NNL.HiddenNode;
 import me.dylan.NNL.Input;
 import me.dylan.NNL.NNetwork;
@@ -9,110 +8,74 @@ import me.dylan.NNL.Synapse;
 import me.dylan.NNL.Utils.NetworkUtil;
 import me.dylan.NNL.Visualizer.Display;
 
+/**
+ * Infinitely looping runnable for use with threads. Runs every 50 milliseconds
+ * 
+ */
 public class MainLoop implements Runnable {
-    private String desiredOutput;
-    public ArrayList<NNetwork> allNetworks = new ArrayList<NNetwork>();
-    private ArrayList<NNetwork> netsSortedByFitness = new ArrayList<NNetwork>();
-    ArrayList<NNetwork> parentNetworks = new ArrayList<NNetwork>();
-    int generation = 1;
+	public ArrayList<NNetwork> allNetworks = new ArrayList<NNetwork>();
+	ArrayList<NNetwork> parentNetworks = new ArrayList<NNetwork>();
+	int generation = 1;
 
-    double avg = 0;
-    ArrayList<Double> similarityPercentages = new ArrayList<Double>();
+	double avg = 0;
+	ArrayList<Double> similarityPercentages = new ArrayList<Double>();
 
-    public MainLoop(String desiredOutput) {
-	this.desiredOutput = desiredOutput;
-    }
-
-    @Override
-    public void run() {
-	while (!Thread.interrupted()) {
-	    if(Display.isKeyDown(' '))
-		continue;
-	    ArrayList<NNetwork> allNetsClone = (ArrayList<NNetwork>) allNetworks
-		    .clone();
-	    for (NNetwork net : allNetsClone) {
-		doMainLoopTick(net);
-
-	    }
-	    try {
-		Thread.sleep(50);
-	    } catch (InterruptedException e) {
-		e.printStackTrace();
-	    }
-	}
-    }
-
-    public void doMainLoopTick(NNetwork net) {
-
-	// timer.start();
-//	net.removeUnusedSynapses();
-	for (Input in : net.getInputNodesInNetwork()) {
-	    in.activateInputNode();
-	}
-	// timer.start();
-	for (HiddenNode n : net.getHiddenNodesInNetwork()) {
-	    n.doTick();
-	}
-	// timer.end();
-	// System.out.println("Hidden Milis: " + timer.getElapsedTimeMilis());
-	// timer.end();
-	// System.out.println(timer.getElapsedTimeMilis());
-	// System.out.println("Paint Milis: " + timer.getElapsedTimeMilis());
-	doGenerationTick(net);
-	for(Synapse synapse : net.getNetworkSynapses()) {
-	    synapse.hasPulsedInTick = false;
-//	    synapse.setPulseBack(false);
-	}
-	for(Input in : net.getInputNodesInNetwork()) {
-	    in.deactivateInputNode();
-	}
-	System.out.println("Network count is " + allNetworks.size()
-		+ " at generation " + generation
-		+ " and the Output fitness is "
-		+ net.getNetworkSimilarityPercentage() + "%");
-	System.out.println("Output: " + net.getNetworkOutput());
-//	 net.getOutputNodesInNetwork().clear();
-//	for (Input in : net.getInputNodesInNetwork()) {
-//	    in.deactiveInputNode();
-//	}
-	try {
-	    Thread.sleep(1000);
-	} catch (InterruptedException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    public void doGenerationTick(NNetwork net) {
-	avg = 0;
-	double matchPercentage = net
-		.getNetworkSimilarityPercentage();
-	if (matchPercentage <= 90) {
-//		net.removeUnusedSynapses();
-//	    if (!netsSortedByFitness.isEmpty()) {
-		NetworkUtil.updateSynapseGenerations(net);
-		/*for (Output out : net.getOutputNodesInNetwork()) {
-		    if (out.getNodeConnections().isEmpty())
-			continue;
-		    for (Synapse synapse : out.traceBackSynapses(true, net)) {
-			if (synapse.getSynapseWeight()
-				+ NNLib.SYNAPSE_MULTIPLIER_PER_GENERATION <= NNLib.MAX_CONNECTION_WEIGHT) {
-
-			    synapse.setSynapseWeight(synapse.getSynapseWeight()
-				    * NNLib.SYNAPSE_MULTIPLIER_PER_GENERATION);
+	@Override
+	public void run() {
+		while (!Thread.interrupted()) {
+			if (Display.isKeyDown(' '))
+				continue;
+			ArrayList<NNetwork> allNetsClone = (ArrayList<NNetwork>) allNetworks
+					.clone();
+			for (NNetwork net : allNetsClone) {
+				doMainLoopTick(net);
 
 			}
-		    }
-		}*/
-		generation++;
-
-//	    } else {
-//		netsSortedByFitness.addAll(allNetworks);
-//	    }
-
-	} else {
-	    System.out.println("Training completed.");
-	    System.out.println(allNetworks.get(0).getNetworkOutput());
-	    System.exit(0);
+			try {
+				Thread.sleep(50); // adjust to change loop time
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-    }
+
+	/**
+	 * Resets the node state by setting the hidden nodes to active, declaring
+	 * that they have not pulsed this tick, and disabling input nodes. Also
+	 * outputs the current amount of networks, the current generation, and the
+	 * current fitness of the network compared to the desired output
+	 * 
+	 * @param parentNet
+	 *            Network to which the nodes belong
+	 */
+	// TODO: MAY NEED TO CHANGE THE INPUT NODE DEACTIVATION FOR WHEN WE COMBINE
+	// TWO NODE VALUES AND FEED IT BACK THROUGH
+	public void doMainLoopTick(NNetwork parentNet) {
+
+		for (Input in : parentNet.getInputNodesInNetwork()) {
+			in.activateInputNode();
+		}
+
+		for (HiddenNode n : parentNet.getHiddenNodesInNetwork()) {
+			n.doTick();
+		}
+
+		for (Synapse synapse : parentNet.getNetworkSynapses()) {
+			synapse.hasPulsedInTick = false;
+		}
+		for (Input in : parentNet.getInputNodesInNetwork()) {
+			in.deactivateInputNode();
+		}
+		System.out.println("Network count is " + allNetworks.size()
+				+ " at generation " + generation
+				+ " and the Output fitness is "
+				+ parentNet.getNetworkSimilarityPercentage() + "%");
+		System.out.println("Output: " + parentNet.getNetworkOutput());
+
+		/*
+		 * try { Thread.sleep(1000); } catch (InterruptedException e) {
+		 * e.printStackTrace(); }
+		 */
+	}
+
 }
--- src/com/Neuroticz/src/Neuroticz.java
+++ src/com/Neuroticz/src/Neuroticz.java
@@ -26,234 +26,221 @@ import me.dylan.NNL.Utils.ThreadUtil;
 import me.dylan.NNL.Visualizer.Display;
 
 public class Neuroticz {
-    Timer timer = new Timer();
-    public static double ZOOM_FACTOR = 1.0;
-    public static final int NETWORKS_ADDITION_PER_GENERATION = 1;
-    public static final int NETWORK_DISPLAY_OFFSET_MULTIPLIER = 300;
-    public static final double HORIZONTAL_NODE_SPACING = 4;
-    public static final double VERTICAL_NODE_SPACING = 2;
-    public static int TOP_SHIFT_INIT = 100;
-    public static Point NODE_SHIFT = new Point(0, TOP_SHIFT_INIT);
-    private static Point NODE_SHIFT_INIT = NODE_SHIFT;
-    private boolean panning = false;
-    MainLoop mainLoop;
-    String desiredOutput = "";
+	Timer timer = new Timer();
+	public static double ZOOM_FACTOR = 1.0;
+	public static final int NETWORKS_ADDITION_PER_GENERATION = 1;
+	public static final int NETWORK_DISPLAY_OFFSET_MULTIPLIER = 300;
+	public static final double HORIZONTAL_NODE_SPACING = 4;
+	public static final double VERTICAL_NODE_SPACING = 2;
+	public static int TOP_SHIFT_INIT = 100;
+	public static Point NODE_SHIFT = new Point(0, TOP_SHIFT_INIT);
+	private static Point NODE_SHIFT_INIT = NODE_SHIFT;
+	private boolean panning = false;
+	MainLoop mainLoop;
+	String desiredOutput = "";
 
-    public Neuroticz() {
+	public Neuroticz() {
 
-	Display.showDisplay("Neuroticz Visualizer", new Dimension(1400, 700),
-		Color.BLACK);
-	Display.setMouseLocation(new Point(Display.getWindowLocOnScreen().x
-		+ Display.getWidth() / 2, Display.getWindowLocOnScreen().y
-		+ Display.getHeight() / 2));
-	Display.addMouseMotionListener(new MouseMotionListener() {
+		Display.showDisplay("Neuroticz Visualizer", new Dimension(1400, 700),
+				Color.BLACK);
+		Display.setMouseLocation(new Point(Display.getWindowLocOnScreen().x
+				+ Display.getWidth() / 2, Display.getWindowLocOnScreen().y
+				+ Display.getHeight() / 2));
+		Display.addMouseMotionListener(new MouseMotionListener() {
 
-	    @Override
-	    public void mouseMoved(MouseEvent evt) {
-		NODE_SHIFT.x = Display.getWidth() / 2 - evt.getX();
-		NODE_SHIFT.y = TOP_SHIFT_INIT + Display.getHeight() / 2
-			- evt.getY();
-	    }
+			@Override
+			public void mouseMoved(MouseEvent evt) {
+				NODE_SHIFT.x = Display.getWidth() / 2 - evt.getX();
+				NODE_SHIFT.y = TOP_SHIFT_INIT + Display.getHeight() / 2
+						- evt.getY();
+			}
 
-	    @Override
-	    public void mouseDragged(MouseEvent evt) {
-		// evt.translatePoint(evt.getComponent().getX(), evt
-		// .getComponent().getY());
-		// panning = true;
-		// NODE_SHIFT.translate((int)(evt.getX() - (NODE_SHIFT.x*2)),
-		// (int)(evt.getY() - (NODE_SHIFT.y*2)));
-		// Point mouseLoc = evt.getPoint();
-		// LEFT_NODE_SHIFT = Display.getWidth() / 2 - (int)
-		// mouseLoc.getX();
-		// TOP_NODE_SHIFT = Display.getWidth() / 2 - (int)
-		// mouseLoc.getY();
-	    }
-	});
+			@Override
+			public void mouseDragged(MouseEvent evt) {
+			}
+		});
 
-	Display.addMouseListener(new MouseListener() {
-	    @Override
-	    public void mouseReleased(MouseEvent e) {
-		if (!panning) {
-		    if (e.getButton() == MouseEvent.BUTTON1) {
-			ZOOM_FACTOR += 0.5;
-		    } else if (e.getButton() == MouseEvent.BUTTON3) {
-			ZOOM_FACTOR -= 0.5;
-		    }
+		Display.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if (!panning) {
+					if (e.getButton() == MouseEvent.BUTTON1) {
+						ZOOM_FACTOR += 0.5;
+					} else if (e.getButton() == MouseEvent.BUTTON3) {
+						ZOOM_FACTOR -= 0.5;
+					}
+				}
+				panning = false;
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+
+		List<File> indata = FileUtil.compileLearningData(new File("recipes"));
+		List<String> tmpLines = new ArrayList<String>();
+		for (File trainingData : indata) {
+			BufferedReader fileIn = null;
+			try {
+				fileIn = new BufferedReader(new FileReader(trainingData));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			String inLine = "";
+			try {
+				while ((inLine = fileIn.readLine()) != null) {
+					if (!inLine.isEmpty() && inLine.trim().length() > 0) {
+						desiredOutput += "\n" + inLine;
+						tmpLines.add(inLine);
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
-		panning = false;
-	    }
+		mainLoop = new MainLoop();
+		for (int i = 0; i < NETWORKS_ADDITION_PER_GENERATION; i++) {
 
-	    @Override
-	    public void mousePressed(MouseEvent e) {
-	    }
-
-	    @Override
-	    public void mouseExited(MouseEvent e) {
-	    }
-
-	    @Override
-	    public void mouseEntered(MouseEvent e) {
-	    }
-
-	    @Override
-	    public void mouseClicked(MouseEvent e) {
-	    }
-	});
-
-	List<File> indata = FileUtil.compileLearningData(new File("recipes"));
-	List<String> tmpLines = new ArrayList<String>();
-	for (File trainingData : indata) {
-	    BufferedReader fileIn = null;
-	    try {
-		fileIn = new BufferedReader(new FileReader(trainingData));
-	    } catch (FileNotFoundException e) {
-		e.printStackTrace();
-	    }
-	    String inLine = "";
-	    try {
-		while ((inLine = fileIn.readLine()) != null) {
-		    if (!inLine.isEmpty() && inLine.trim().length() > 0) {
-			desiredOutput += "\n" + inLine;
-			tmpLines.add(inLine);
-		    }
+			NNetwork initialNet = NetworkUtil.initializeNetwork(0, 1, 1,
+					desiredOutput, tmpLines);
+			// initialNet.addOutputNodeToNetwork(new Output());
+			// Input inputNode = new Input(desiredOutput.split("\n"));
+			// initialNet.addInputNodeToNetwork(inputNode);
+			if (TestUtil.AnyNodesExist(initialNet)) {
+				TestUtil.WhatNodesExist(initialNet);
+			}
+			System.out.println("Any hidden nodes have values assigned? "
+					+ TestUtil.AHiddenNodeHasValue(initialNet));
+			System.out.println("Are all synapses properly assigned? "
+					+ TestUtil.IsSynapseCountProper(initialNet));
+			// initialNet.connectAll();
+			mainLoop.allNetworks.add(initialNet);
 		}
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
+		// net.randomizeConnections();
+		ThreadUtil.spinThreadForPool("mainLoop", mainLoop);
+		ThreadUtil.spinThreadForPool("drawThread", new Runnable() {
+			@Override
+			public void run() {
+				while (!Thread.interrupted()) {
+					ArrayList<NNetwork> networkClones = (ArrayList<NNetwork>) mainLoop.allNetworks
+							.clone();
+					int netcount = 0;
+					for (NNetwork net : networkClones) {
 
+						netcount++;
+						Display.fillRect(0, 0, Display.getWidth(),
+								Display.getHeight());
+						Display.setOffset(new Point(netcount
+								* Neuroticz.NETWORK_DISPLAY_OFFSET_MULTIPLIER,
+								0));
+						draw(net);
+						Display.setOffset(new Point(-netcount
+								* Neuroticz.NETWORK_DISPLAY_OFFSET_MULTIPLIER,
+								0));
+					}
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 	}
-	mainLoop = new MainLoop(desiredOutput);
-	for (int i = 0; i < NETWORKS_ADDITION_PER_GENERATION; i++) {
 
-	    NNetwork initialNet = NetworkUtil.initializeNetwork(0, 1, 1,
-		    desiredOutput, tmpLines);
-	    // initialNet.addOutputNodeToNetwork(new Output());
-	    // Input inputNode = new Input(desiredOutput.split("\n"));
-	    // initialNet.addInputNodeToNetwork(inputNode);
-	    if (TestUtil.AnyNodesExist(initialNet)) {
-		TestUtil.WhatNodesExist(initialNet);
-	    }
-	    System.out.println("Any hidden nodes have values assigned? "
-		    + TestUtil.AHiddenNodeHasValue(initialNet));
-	    System.out.println("Are all synapses properly assigned? "
-		    + TestUtil.IsSynapseCountProper(initialNet));
-	    // initialNet.connectAll();
-	    mainLoop.allNetworks.add(initialNet);
+	public static void main(String args[]) {
+		new Neuroticz();
 	}
-	// net.randomizeConnections();
-	ThreadUtil.spinThreadForPool("mainLoop", mainLoop);
-	ThreadUtil.spinThreadForPool("drawThread", new Runnable() {
-	    @Override
-	    public void run() {
-		while (!Thread.interrupted()) {
-		    ArrayList<NNetwork> networkClones = (ArrayList<NNetwork>) mainLoop.allNetworks
-			    .clone();
-		    int netcount = 0;
-		    for (NNetwork net : networkClones) {
 
-			netcount++;
-			Display.fillRect(0, 0, Display.getWidth(),
-				Display.getHeight());
-			Display.setOffset(new Point(netcount
-				* Neuroticz.NETWORK_DISPLAY_OFFSET_MULTIPLIER,
-				0));
-			draw(net);
-			Display.setOffset(new Point(-netcount
-				* Neuroticz.NETWORK_DISPLAY_OFFSET_MULTIPLIER,
-				0));
-		    }
-		    try {
-			Thread.sleep(10);
-		    } catch (InterruptedException e) {
-			e.printStackTrace();
-		    }
+	public void draw(NNetwork network) {
+		drawSynapses(network);
+		int row = 0;
+		ArrayList<Input> netInputNodes = (ArrayList<Input>) network
+				.getInputNodesInNetwork().clone();
+		for (Input in : netInputNodes) {
+			double x = NODE_SHIFT.x
+					+ 130
+					+ (row * Node.NODE_DRAW_SIZE * HORIZONTAL_NODE_SPACING * ZOOM_FACTOR);
+			in.paint((int) x, (int) ((Node.NODE_DRAW_SIZE * 2)
+					* VERTICAL_NODE_SPACING * ZOOM_FACTOR)
+					+ NODE_SHIFT.y);
+			row++;
 		}
-	    }
-	});
-    }
+		int y = 1;
+		row = 0;
+		ArrayList<HiddenNode> netHiddenNodes = (ArrayList<HiddenNode>) network
+				.getHiddenNodesInNetwork().clone();
+		for (HiddenNode n : netHiddenNodes) {
+			y += 1;
+			double x = NODE_SHIFT.x
+					+ 180
+					+ (row * Node.NODE_DRAW_SIZE * HORIZONTAL_NODE_SPACING * ZOOM_FACTOR);
+			n.paint((int) x, (int) (y * (Node.NODE_DRAW_SIZE * 2)
+					* VERTICAL_NODE_SPACING * ZOOM_FACTOR)
+					+ NODE_SHIFT.y);
+			if (network.getHiddenNodesInNetwork().indexOf(n) % 10 == 0) {
+				row++;
+				y = 1;
+			}
 
-    public static void main(String args[]) {
-	new Neuroticz();
-    }
+		}
+		row = 0;
+		for (Output out : network.getOutputNodesInNetwork()) {
+			double x = NODE_SHIFT.x
+					+ 180
+					+ 200
+					+ (row * Node.NODE_DRAW_SIZE * HORIZONTAL_NODE_SPACING * ZOOM_FACTOR);
 
-    public void draw(NNetwork network) {
-	drawSynapses(network);
-	int row = 0;
-	ArrayList<Input> netInputNodes = (ArrayList<Input>) network
-		.getInputNodesInNetwork().clone();
-	for (Input in : netInputNodes) {
-	    double x = NODE_SHIFT.x
-		    + 130
-		    + (row * Node.NODE_DRAW_SIZE * HORIZONTAL_NODE_SPACING * ZOOM_FACTOR);
-	    in.paint((int) x, (int) ((Node.NODE_DRAW_SIZE * 2)
-		    * VERTICAL_NODE_SPACING * ZOOM_FACTOR)
-		    + NODE_SHIFT.y);
-	    row++;
+			out.paint((int) x, (int) (Node.NODE_DRAW_SIZE * 20
+					* VERTICAL_NODE_SPACING * ZOOM_FACTOR)
+					+ NODE_SHIFT.y);
+			row++;
+		}
 	}
-	int y = 1;
-	row = 0;
-	ArrayList<HiddenNode> netHiddenNodes = (ArrayList<HiddenNode>) network
-		.getHiddenNodesInNetwork().clone();
-	for (HiddenNode n : netHiddenNodes) {
-	    y += 1 /* + network.getHiddenNodesInNetwork().indexOf(n) % 4 */;
-	    double x = NODE_SHIFT.x
-		    + 180
-		    + (row * Node.NODE_DRAW_SIZE * HORIZONTAL_NODE_SPACING * ZOOM_FACTOR);
-	    n.paint((int) x, (int) (y * (Node.NODE_DRAW_SIZE * 2)
-		    * VERTICAL_NODE_SPACING * ZOOM_FACTOR)
-		    + NODE_SHIFT.y);
-	    if (network.getHiddenNodesInNetwork().indexOf(n) % 10 == 0) {
-		row++;
-		y = 1;
-	    }
-	    // if(column >= 2) {
-	    // column+=10;
-	    // }
 
+	public void drawSynapses(NNetwork net) {
+		// net.removeUnusedSynapses();
+		ArrayList<Synapse> synapsesClone = (ArrayList<Synapse>) net
+				.getNetworkSynapses().clone();
+		for (Synapse connection : synapsesClone) {
+			if (connection == null
+					|| connection.getConnectionDestination() == null
+					|| connection.getConnectionOrigin() == null)
+				continue;
+			Point originDrawingPoint = connection.getConnectionOrigin().graphicsRepresentationObject
+					.getPaintCoords();
+			Point destinationDrawingPoint = connection
+					.getConnectionDestination().graphicsRepresentationObject
+					.getPaintCoords();
+			Color displayColor = Display.getDisplayBackgroundColor();
+			Display.setDisplayBackgroundColor(NetworkUtil
+					.returnWeightColor(connection.getSynapseWeight()));
+			// if(connection.hasPulsedInTick)
+			// Display.setDisplayBackgroundColor(Color.MAGENTA);
+			// else if(connection.hasPulsedInTick)
+			// Display.setDisplayBackgroundColor(Color.BLUE);
+			Display.drawLine(originDrawingPoint.x, originDrawingPoint.y,
+					destinationDrawingPoint.x, destinationDrawingPoint.y);
+			Display.setDisplayBackgroundColor(displayColor);
+
+			// } else {
+			//
+			// }
+
+		}
 	}
-	row = 0;
-	for (Output out : network.getOutputNodesInNetwork()) {
-	    double x = NODE_SHIFT.x
-		    + 180
-		    + 200
-		    + (row * Node.NODE_DRAW_SIZE * HORIZONTAL_NODE_SPACING * ZOOM_FACTOR);
-
-	    out.paint((int) x, (int) (Node.NODE_DRAW_SIZE * 20
-		    * VERTICAL_NODE_SPACING * ZOOM_FACTOR)
-		    + NODE_SHIFT.y);
-	    row++;
-	}
-    }
-
-    public void drawSynapses(NNetwork net) {
-	// net.removeUnusedSynapses();
-	ArrayList<Synapse> synapsesClone = (ArrayList<Synapse>) net
-		.getNetworkSynapses().clone();
-	for (Synapse connection : synapsesClone) {
-	    if (connection == null
-		    || connection.getConnectionDestination() == null
-		    || connection.getConnectionOrigin() == null)
-		continue;
-	    Point originDrawingPoint = connection.getConnectionOrigin().graphicsRepresentationObject
-		    .getPaintCoords();
-	    Point destinationDrawingPoint = connection
-		    .getConnectionDestination().graphicsRepresentationObject
-		    .getPaintCoords();
-	    Color displayColor = Display.getDisplayBackgroundColor();
-	    Display.setDisplayBackgroundColor(NetworkUtil
-		    .returnWeightColor(connection.getSynapseWeight()));
-	    // if(connection.hasPulsedInTick)
-	    // Display.setDisplayBackgroundColor(Color.MAGENTA);
-	    // else if(connection.hasPulsedInTick)
-	    // Display.setDisplayBackgroundColor(Color.BLUE);
-	    Display.drawLine(originDrawingPoint.x, originDrawingPoint.y,
-		    destinationDrawingPoint.x, destinationDrawingPoint.y);
-	    Display.setDisplayBackgroundColor(displayColor);
-
-	    // } else {
-	    //
-	    // }
-
-	}
-    }
 }
