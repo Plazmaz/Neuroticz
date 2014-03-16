8e20ea9e8238183f8fb1785985797dfffcbb9698
Craig Gleckman <cgleckman@gmail.com>
2014-03-16 15:52:29 -0700

    Revert "Review"
    This reverts commit 04ce37fa21132588eeeddfa7b84c423ed6383221.

--- src/com/Neuroticz/src/Timer.java
+++ src/com/Neuroticz/src/Timer.java
@@ -1,29 +1,30 @@
 package com.Neuroticz.src;
 
 public class Timer {
-	long start;
-	long end;
+    long start;
+    long end;
 
-	/**
-	 * Start the timer
-	 */
-	public void start() {
-		start = System.currentTimeMillis();
-	}
+    public Timer() {
+    }
+    /**
+     * 
+     */
+    public void start() {
+	start = System.currentTimeMillis();
+    }
+    /**
+     * 
+     */
+    public void end() {
+	end = System.currentTimeMillis();
+    }
 
-	/**
-	 * End the timer
-	 */
-	public void end() {
-		end = System.currentTimeMillis();
-	}
-
-	/**
-	 * Determines the total time that has passed since the Timer was started
-	 * 
-	 * @return the total time passed in milliseconds
-	 */
-	public long getElapsedTimeMilis() {
-		return end - start;
-	}
+    /**
+     * Determines the total time that has passed since the Timer was started
+     * @return the total time passed in milliseconds
+     */
+    //TODO: Can we convert this to seconds/minutes/hours somewhere?
+    public long getElapsedTimeMilis() {
+	return end - start;
+    }
 }
